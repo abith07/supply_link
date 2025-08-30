dea98cae6fa7f226604c79637aa19b4d068880ce
abith07 <163716690+abith07@users.noreply.github.com>
2025-08-30 17:46:10 +0530

    Update Warehouse.java

--- backend/src/main/java/com/wecp/progressive/entity/Warehouse.java
+++ backend/src/main/java/com/wecp/progressive/entity/Warehouse.java
@@ -1,13 +1,16 @@
 package com.wecp.progressive.entity;
 
 public class Warehouse {
+
     private int warehouseId;
     private int supplierId;
     private String warehouseName;
     private String location;
     private int capacity;
+
     public Warehouse() {
     }
+
     public Warehouse(int warehouseId, int supplierId, String warehouseName, String location, int capacity) {
         this.warehouseId = warehouseId;
         this.supplierId = supplierId;
@@ -15,36 +18,44 @@ public class Warehouse {
         this.location = location;
         this.capacity = capacity;
     }
+
     public int getWarehouseId() {
         return warehouseId;
     }
+
     public void setWarehouseId(int warehouseId) {
         this.warehouseId = warehouseId;
     }
+
     public int getSupplierId() {
         return supplierId;
     }
+
     public void setSupplierId(int supplierId) {
         this.supplierId = supplierId;
     }
+
     public String getWarehouseName() {
         return warehouseName;
     }
+
     public void setWarehouseName(String warehouseName) {
         this.warehouseName = warehouseName;
     }
+
     public String getLocation() {
         return location;
     }
+
     public void setLocation(String location) {
         this.location = location;
     }
+
     public int getCapacity() {
         return capacity;
     }
+
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
-    
-
 }
