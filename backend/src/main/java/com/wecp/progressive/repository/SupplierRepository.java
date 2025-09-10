145a1eb664c9dfa75711a80c6575c82f34e84089
Abith Mohammed <163716690+abith07@users.noreply.github.com>
2025-09-10 22:56:01 +0530

    Update SupplierRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
@@ -1,22 +1,18 @@
 package com.wecp.progressive.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.wecp.progressive.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import com.wecp.progressive.entity.Supplier;
-import com.wecp.progressive.service.impl.SupplierServiceImplJdbc;
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
 
-public interface SupplierRepository extends JpaRepository<Supplier,Integer>{
-    
-    
+    void deleteBySupplierId(@Param("supplierId") int supplierId);
 
-    public Supplier deleteBySupplierId(@Param("supplierId") int supplierId);
+    Supplier findBySupplierId(@Param("supplierId") int supplierId);
 
-    public Supplier findBySupplierId(@Param("supplierId") int supplierId);
+    Supplier findByUsername(String username);
 
-    public Object findByEmail(String email);
-
-    public Object findByUsername(String username);
-
+    Supplier findByEmail(String email);
 }
