e4f579a6c98713777d892c9a074f47a53d93c41d
Abith Mohammed <163716690+abith07@users.noreply.github.com>
2025-09-20 22:15:19 +0530

    Update supplylink.module.ts

--- frontend/src/app/supplylink/supplylink.module.ts
+++ frontend/src/app/supplylink/supplylink.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from "@angular/common";
 import { SupplyLinkRoutingModule } from "./supplylink-routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { ProductComponent } from "./components/product/product.component";
+import { SupplierComponent } from "./components/supplier/supplier.component";
+import { WarehouseComponent } from "./components/warehouse/warehouse.component";
 
 @NgModule({
   declarations: [],
@@ -11,7 +14,7 @@ import { HttpClientModule } from "@angular/common/http";
     CommonModule,
     SupplyLinkRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule,
+    HttpClientModule
   ],
   exports: [
     
